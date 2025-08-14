7c4ef016a69aa3dd667b6225af93ecc9aae29a23
kamstand <kamrul.hassan@stonybrook.edu>
2025-08-14 16:46:09 -0400

    started on app frontend

--- smart-pos-frontend/src/app/app.ts
+++ smart-pos-frontend/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
+interface product {
+  
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('smart-pos-frontend');
+
 }
